c97896ef0e91037fc94d522aa9325d1748360ce5
rafael <rrrr0ffff@gmail.com>
2023-04-04 19:39:50 -0300

    start working on models and controllers

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import index from "./routes/index";
 
 dotenv.config();
 
@@ -13,9 +14,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("Hello, Fábrica de Startups");
-});
+app.get("/", index);
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
