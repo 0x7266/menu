013ddaf006bb4b759cec7ab727ce82f5551c2df0
rafael <rrrr0ffff@gmail.com>
2023-04-04 21:45:18 -0300

    added types

---
+++ src/types/Category.ts
@@ -0,0 +1,6 @@
+type Category = {
+	parent: Category | null;
+	name: String;
+};
+
+export default Category;
---
+++ src/types/Product.ts
@@ -0,0 +1,10 @@
+import Category from "./Category";
+
+type Product = {
+	categories: Category[];
+	name: String;
+	qty: Number;
+	price: Number;
+};
+
+export default Product;
