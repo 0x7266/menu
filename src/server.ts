5a23464ca41d7f67a4468d3177e0326aa17a6aae
rafael <rrrr0ffff@gmail.com>
2023-04-06 02:55:20 -0300

    docs

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import swagger from "swagger-ui-express";
+import swaggerDocs from "./config/swagger.json";
 import index from "./routes/index";
 import { connect } from "./config/database";
 
@@ -13,6 +15,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerDocs)); // rota para ver a documentação da api
 
 app.use("/", index);
 app.get("*", (req, res) => {
